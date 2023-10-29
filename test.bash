97e194f0f1c60a6360a51cd2d753c5b529c5a94d
Yuya Koyama <vulpes.yuya@gmail.com>
2023-10-30 00:09:58 +0900

    20231030 change

--- test.bash
+++ test.bash
@@ -1,5 +1,18 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2023 Yuya Kyama
+# SPDX-License-Identifier: BSD-3-Clause
 
+ng(){
+        echo NG at Line $1
+        res=1
+}
+
+res=0
+
+### I/O TEST###
 out=$(seq 5 | ./plus)
+[ "$a" = 14 ] || ng ${LINENO}
 
-[ "${out}" = 14 ]
+[ "$res" = 0 ] && echo OK         # &&(AND記号) は左側が成功すると右側を実行
+
+exit $res
