60fa66bb681c4d2248dcdcff31d36748554b7496
Yuya Koyama <vulpes.yuya@gmail.com>
2023-11-28 00:43:50 +0900

    change test.bash

--- test.bash
+++ test.bash
@@ -15,10 +15,42 @@ res=0
 out=$(seq 5 | ./plus)
 [ "${out}" = 15 ] || ng ${LINENO}
 
+
 #minus
 out=$(seq 5 | ./minus)
 [ "${out}" = -15 ] || ng ${LINENO}
 
-[ "$res" = 0 ] && echo OK         # &&(AND記号) は左側が成功すると右側を実行
+### STRANGE INPUT ###
 
+#plus
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+    　 
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+
+#minus
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+    　 
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+
+#mul
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+    　 
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]      || ng ${LINENO}
+[ "${out}" = "" ] || ng ${LINENO}
+
+
+[ "$res" = 0 ] && echo OK         # &&(AND記号) は左側が成功すると右側を実行
 exit $res
